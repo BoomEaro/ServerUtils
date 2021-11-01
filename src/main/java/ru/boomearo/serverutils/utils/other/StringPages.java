7134156807e8eb8671496a9ff83a0864e1942a60
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2021-11-01 14:02:07 +0300

    Add old sendPageInfo

--- src/main/java/ru/boomearo/serverutils/utils/other/StringPages.java
+++ src/main/java/ru/boomearo/serverutils/utils/other/StringPages.java
@@ -40,6 +40,38 @@ public class StringPages {
         sender.sendMessage(sep);
     }
 
+    public static void sendPageInfo(CommandSender sender, List<String> data, int page, int pageLimit, PageInfo info) {
+        String prefix = info.getPrefix();
+        if (prefix == null) {
+            prefix = "";
+        }
+
+        if (page <= 0) {
+            sender.sendMessage(prefix + "Указанная страница должна быть больше нуля.");
+            return;
+        }
+        int offSet = (page - 1) * pageLimit;
+        if (offSet >= data.size()) {
+            sender.sendMessage(prefix + "Указанная страница не найдена.");
+            return;
+        }
+
+        int maxPage = data.size() / pageLimit + (data.size() % pageLimit > 0 ? 1 : 0);
+
+        final String sep = prefix + "§8==========================";
+
+        sender.sendMessage(sep);
+        sender.sendMessage(prefix + info.getCurrentPageInfo(page, maxPage));
+        for (int i = 0; i < pageLimit; i++) {
+            int newO = offSet + i;
+            if (newO >= data.size()) {
+                break;
+            }
+            sender.sendMessage(prefix + info.getDataFormat((newO + 1)));
+        }
+        sender.sendMessage(sep);
+    }
+
     public static interface PageInfo {
 
         public String getPrefix();
