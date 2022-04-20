1792f39165db325647e6fbc8021d765f8f2569e2
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-04-20 17:06:46 +0300

    Add tps runnable from Board

--- src/main/java/ru/boomearo/serverutils/ServerUtils.java
+++ src/main/java/ru/boomearo/serverutils/ServerUtils.java
@@ -5,10 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.boomearo.serverutils.commands.serverutils.ServerUtilsExecutor;
 import ru.boomearo.serverutils.listeners.CommandsListener;
 import ru.boomearo.serverutils.utils.own.NetworkWatcher;
+import ru.boomearo.serverutils.utils.own.TpsRunnable;
 
 public class ServerUtils extends JavaPlugin {
 
     private final NetworkWatcher watcher = new NetworkWatcher();
+    private TpsRunnable tpsRunnable = null;
 
     private static ServerUtils instance = null;
 
@@ -30,6 +32,14 @@ public class ServerUtils extends JavaPlugin {
         this.watcher.unregister();
 
         getLogger().info("Плагин успешно выключен");
+    }
+
+    public TpsRunnable getTpsRunnable() {
+        if (this.tpsRunnable == null) {
+            this.tpsRunnable = new TpsRunnable();
+        }
+
+        return this.tpsRunnable;
     }
 
     public static ServerUtils getInstance() {
